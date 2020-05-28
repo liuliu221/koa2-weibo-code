568067273470366203bb5f7078f398459a97f684
unknown <32315349@qq.com>
2020-05-28 16:58:23 +0800

    post 请求

--- src/routes/index.js
+++ src/routes/index.js
@@ -16,4 +16,22 @@ router.get('/json', async (ctx, next) => {
   }
 })
 
+
+router.get('/profile/:username', async (ctx,next)=>{
+     const { username } = ctx.params
+     ctx.body={
+         title:"koa2 json",
+        username
+     }
+})
+
+router.get('/loadMore/:userName/:pageIndex', async (ctx,next) =>{
+      const { userName, pageIndex} =ctx.params
+        ctx.body={
+            title:"koa2 json",
+            userName,
+            pageIndex
+        }
+})
+
 module.exports = router
